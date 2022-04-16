5ddcaa0690044e4cb8d32efcc53b621a1fef1a1c
sebastian-telstra <99169134+sebastian-telstra@users.noreply.github.com>
2022-04-16 11:11:15 +0800

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const os = require("os");
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
 const hostname = os.hostname();
